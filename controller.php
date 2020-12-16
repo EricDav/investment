bd473dc93da28c541a8770c1063e7a3f6bab3e4b
David Alienyi <alienyidavid4christ@gmail.com>
2020-12-16 10:16:00 +0100

    fix update bug

--- controller.php
+++ controller.php
@@ -94,7 +94,7 @@
            $user['password'] = '';
            $_SESSION['userInfo'] = $user;
            $tokenCode = Helper::generatePin();
-            if (Model::update($pdo, array('code_token'=> $tokenCode), 'users')) {
+            if (Model::update($pdo, array('code_token'=> $tokenCode), array('id' => $user['id']), 'users')) {
                 Helper::jsonResponse(array('success' => true, 'message' => 'logged in successfully', 'id' => $user['id'], 'code' => $tokenCode));
             }
         }
